bdcfb2892ff985c9c790f499bae64b8c27c2d09d
zainalmuttaqin <145755754+zainalmuttaqin@users.noreply.github.com>
2023-09-29 16:25:31 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya ZAINAL MUTTAQIN saya tinggal di Solo dan Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
